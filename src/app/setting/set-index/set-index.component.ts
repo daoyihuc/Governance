7888042f698f514b8c636c59c794212401337f03
sunth <1798505428@qq.com>
2020-12-17 23:02:05 +0800

    设置

--- src/app/setting/set-index/set-index.component.ts
+++ src/app/setting/set-index/set-index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {log} from "util";
 
 @Component({
   selector: 'app-set-index',
@@ -8,8 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class SetIndexComponent implements OnInit {
 
   constructor() { }
-
+  lists = [
+      {name: '用户名', text: 'xa548153'},
+      {name: '姓名', text: '徐璈'},
+      {name: '所属单位', text: '第二机动大队'}
+      ];
+  btns = [
+    {text: '修改密码'},
+    {text: '系统信息'},
+    {text: '注销账号'}
+  ];
+  clickBtn = '';
   ngOnInit(): void {
+  }
+
+  onBtn(data): void {
+    console.log(data);
+    this.clickBtn = data.text;
   }
 
 }
