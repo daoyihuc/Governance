12e42386091056e20e60428929a9d9757153a1cf
sunth <1798505428@qq.com>
2020-12-23 00:35:57 +0800

    c2源头企业-1+2+3

--- src/app/command/command-router.module.ts
+++ src/app/command/command-router.module.ts
@@ -5,6 +5,9 @@ import {CommandIndexComponent} from "./command-index/command-index.component";
 import {BasicInformationComponent} from "./basic-information/basic-information.component";
 import {FeedbackComponent} from './feedback/feedback.component';
 import {CommandCarComponent} from "./command-car/command-car.component";
+import {WeighingComponent} from "./weighing/weighing.component";
+import {MonitoringComponent} from "./monitoring/monitoring.component";
+import {EnterpriseDetailsComponent} from "./enterprise-details/enterprise-details.component";
 
 const commandRouter: Routes = [
   {
@@ -20,6 +23,9 @@ const commandRouter: Routes = [
   {path: 'basicInformation', component: BasicInformationComponent},
   {path: 'feedback', component: FeedbackComponent},
   {path: 'commandCar', component: CommandCarComponent},
+  {path: 'weighing', component: WeighingComponent},
+  {path: 'monitoring', component: MonitoringComponent},
+  {path: 'enterpriseDetails', component: EnterpriseDetailsComponent},
   { path: '', redirectTo: 'commandIndex', pathMatch: 'full' },
   { path: '**', redirectTo: 'commandIndex' },
 ];
