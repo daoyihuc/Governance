3986b28ae463355b836620f3cf1d0b98b8f689b4
sunth <1798505428@qq.com>
2020-12-26 00:41:35 +0800

    new

---
+++ src/app/command/inspector/inspector.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {WindowService} from "../../utils/window.service";
+
+@Component({
+  selector: 'app-inspector',
+  templateUrl: './inspector.component.html',
+  styleUrls: ['./inspector.component.scss']
+})
+export class InspectorComponent implements OnInit {
+
+  constructor(
+    private route: Router, // 路由传递
+    private router: ActivatedRoute, // 路由接收者
+    private windowUntils: WindowService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onBack(): void {
+    this.windowUntils.onBack();
+  }
+
+  onHome(): void {
+    this.route.navigate(['/home']);
+  }
+
+  onSetting(): void { // 个人中心
+    this.route.navigate(['/setting']);
+  }
+
+  onJump(): void{// 跳转
+    this.windowUntils.onBack();
+  }
+
+}
